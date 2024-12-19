3d598a68a289997c734bf76ad31937a917ec433c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-12-19 10:42:51 +0530

    fix

--- src/db/db_utils.ts
+++ src/db/db_utils.ts
@@ -2,9 +2,10 @@ import { z } from 'zod';
 
 export const get_db_url = (env: any): string => {
   let url: string = null!;
-  if (process.env.DB_MODE === 'PROD') url = env.PG_DATABASE_URL1;
-  else if (process.env.DB_MODE === 'PREVIEW') url = env.PG_DATABASE_URL2;
-  else url = env.PG_DATABASE_URL;
+  if (typeof process !== 'undefined') {
+    if (process.env.DB_MODE === 'PROD') url = env.PG_DATABASE_URL1;
+    else if (process.env.DB_MODE === 'PREVIEW') url = env.PG_DATABASE_URL2;
+  } else url = env.PG_DATABASE_URL;
   const url_parse = z
     .string({
       description: 'Connection string for PostgreSQL'
